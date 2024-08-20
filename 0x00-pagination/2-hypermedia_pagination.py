9f969e04fd0764c94230d9b41c953fb0148583b1
tamerkat <mahad10taha@gmail.com>
2024-08-20 14:41:32 +0300

    add

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python3
-"""Main file"""
-
-
+"""Simple pagination."""
 import csv
 import math
-from typing import List, Dict
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """main def"""
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return start_index, end_index
+from typing import List, Tuple
 
 
 class Server:
@@ -34,28 +25,38 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0 
-        assert isinstance(page_size, int) and page_size > 0 
-        
-        start_index, end_index = index_range(page, page_size)
-        
-        dataset = self.dataset()
-        if start_index > len(dataset):
-            return []
-        
-        return dataset[start_index : end_index]
+        """Return list of rows from dataset."""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
-        """Return a dictionary with pagination details."""
-        data = self.get_page(page, page_size)
-        total_items = len(self.dataset())
-        total_pages = math.ceil(total_items / page_size)
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
 
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return dict of pagination info."""
+        total_pages = math.ceil(len(self.dataset()) / page_size)
         return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if page < total_pages else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_pages": total_pages,
+            'page_size': len(self.get_page(page, page_size)),
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
         }
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return tuple of size two containing a start index and an end index."""
+    return ((page - 1) * page_size, page * page_size)
+
+
+if __name__ == "__main__":
+    server = Server()
+
+    print(server.get_hyper(1, 2))
+    print("---")
+    print(server.get_hyper(2, 2))
+    print("---")
+    print(server.get_hyper(100, 3))
+    print("---")
+    print(server.get_hyper(3000, 100))
