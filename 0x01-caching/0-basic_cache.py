e43beec5ff210f37bc8036415a62b9d665c169a2
tamerkat <mahad10taha@gmail.com>
2024-08-21 14:25:12 +0300

    add

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,46 @@
 #!/usr/bin/python3
-""" BaseCaching module"""
-
-
-from base_caching import BaseCaching
+"""BasicCache module.
+"""
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """class"""
+    """BasicCache class.
+    """
+
     def __init__(self):
-        """Constructor."""
+        """Constructor.
+        """
         super().__init__()
 
     def put(self, key, item):
-        """function"""
+        """Put method.
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-    """function"""
-    if key in self.cache_data:
-        return self.cache_data[key]
-    return None
+        """Get method.
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
+
+
+if __name__ == "__main__":
+    my_cache = BasicCache()
+    my_cache.print_cache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    print(my_cache.get("D"))
+    my_cache.print_cache()
+    my_cache.put("D", "School")
+    my_cache.put("E", "Battery")
+    my_cache.put("A", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
