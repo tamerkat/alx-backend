2c319b934c4eb9cd355a0ca3ecd21b40a5ba09ea
tamerkat <mahad10taha@gmail.com>
2024-08-21 18:02:47 +0300

    add

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""LIFO caching.
+"""LRU Caching.
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LIFOCache class.
+    """LRUCache class.
     """
 
     def __init__(self):
@@ -21,7 +21,7 @@ class LRUCache(BaseCaching):
             if key in self.cache_data:
                 self.queue.remove(key)
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discard = self.queue.pop()
+                discard = self.queue.pop(0)
                 del self.cache_data[discard]
                 print("DISCARD: {}".format(discard))
             self.queue.append(key)
@@ -39,3 +39,28 @@ class LRUCache(BaseCaching):
 
 if __name__ == "__main__":
     my_cache = LRUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
